e7c3bb278e0e1d1cc1acd8b103209a90ae868054
lecuberfou <44326022+lecuberfou@users.noreply.github.com>
2020-08-07 15:01:14 +0200

    setFile, constructor & saver example

--- src/index.js
+++ src/index.js
@@ -4,18 +4,40 @@ fetch('https://raw.githubusercontent.com/Stuk/jszip/master/dist/jszip.min.js')
         .then(function (text) { eval(text); });
     });
 
-const MinecraftCompiler = class MinecraftCompiler {
-	static compile (code) {
-		if ([...arguments].length < 1) {
-			throw new TypeError('Failed to compile : 1 argument required, but only 0 present.');
-		}
+MinecraftCompiler = class MinecraftCompiler {
+
+	constructor () {
+		this.files = {};
+
+		this.namespace = 'mccompiler';
+	}
+
+	setFile (name, content) {
+		if ([...arguments].length < 2) { throw new TypeError(`Failed to compile : 2 arguments required, but only ${[...arguments].length} present.`); }
 		
-		if (typeof code !== 'string') {
-			throw new TypeError('Failed to compile : Expect argument 1 to be a String');
-		}
-		
-		// do some computation
-		
-		return true;
+		if (typeof name !== 'string') { throw new TypeError('Failed to compile : Expect argument 1 (name) to be a String'); }
+		if (typeof content !== 'string') { throw new TypeError('Failed to compile : Expect argument 2 (content) to be a String'); }
+
+		this.files[name] = content;
+	}
+
+	compile () {
+		return new Promise (function (resolve, reject) {
+
+			var zip = new JSZip();
+
+			zip.file('pack.mcmeta', `{"pack": {"pack_format": 5,"description": "The default data for Minecraft"}}`);
+
+			/*zip.file(`data/${this.namespace}/tags/functions/`);
+			zip.file(`data/${this.namespace}/tags/functions/`);*/
+			zip.file(`data/${this.namespace}/functions/test.mcfunction`, "say Hello world!");
+
+			var functions = {};
+			
+			// do some computation
+			
+			zip.generateAsync({type:'blob'}).then(resolve);
+
+		}.bind(this));
 	}
 }
